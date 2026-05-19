c35251a5cacd14b5939743f3000a74b0e8c5f352
kaiyu <huangkaiyu0522@gmail.com>
2026-05-19 17:58:05 +0800

    add module back

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module FinalProject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // 開放你的 package 讓 JavaFX 系統讀取（假設你的 main 在 com.example 套件下）
+    opens com.example.finalproject to javafx.fxml;
+    exports com.example.finalproject;
+}
