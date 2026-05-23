a1b5a33ac7404c1dba62a67e870109f3e1b0fffe
kaiyu <huangkaiyu0522@gmail.com>
2026-05-23 18:00:22 +0800

    增加背景、簡單音效、聲音選單

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module FinalProject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     // 開放你的 package 讓 JavaFX 系統讀取（假設你的 main 在 com.example 套件下）
     opens com.example.finalproject to javafx.fxml;
